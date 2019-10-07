8d3e77282e8a8e39a111ec7c97195453e22d0811
Erik Nilsson <eriknilsson@lnor034114.corp.capgemini.com>
2019-10-07 14:50:56 +0200

    Adding pause before currentTime to zero

--- index.js
+++ index.js
@@ -17,16 +17,19 @@ window.addEventListener('load', () => {
                 switch (true) {
                     case button.id === "hell":
                         const hell = document.getElementById("hell-sound");
+                        hell.pause();
                         hell.currentTime = 0;
                         hell.play();
                     break;
                     case button.id === "shut":
                         const shut = document.getElementById("shut-sound");
+                        shut.pause();
                         shut.currentTime = 0;
                         shut.play();
                     break;
                     case button.id === "kiss":
                         const kiss = document.getElementById("kiss-sound");
+                        kiss.pause();
                         kiss.currentTime = 0;
                         kiss.play();
                     break;
